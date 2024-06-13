8fa453c367f0734e439554f50e2159c4f2377986
mohasina.shaik <mohasina.shaik@archents.com>
2024-06-13 13:07:52 +0530

    Initial Commit for the Login Page

--- src/Components/Users/Login.jsx
+++ src/Components/Users/Login.jsx
@@ -1,3 +1,51 @@
-export default function Login() {
-  return <div>Login</div>;
-}
+import React from 'react';
+import {Helmet} from 'react-helmet';
+import { useNavigate } from 'react-router-dom';
+import '../../assets/Styles/Login.css'
+
+
+const Home = (props) => {
+  const { loggedIn, email } = props;
+  const navigate = useNavigate();
+
+  const onButtonClick = () => {
+  };
+
+  return (
+    <div className="mainContainer">
+        <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      {/* Logo */}
+      <img src="src/assets/Images/ArchentsLogo.png" alt="Logo" className="logo" /> 
+
+      <div className="welcomeMessage">
+        <div id='welcome'>Welcome back!</div>
+        <div className="Text">Please login using your account.</div>
+      </div>
+
+      {/* Login form */}
+      <div className="loginForm">
+        <input type="text" placeholder="Email Address" className="inputField" />
+        <input type="password" placeholder="Password" className="inputField" />
+        <button onClick={onButtonClick} className="loginButton">
+          Login
+        </button>
+      </div>
+
+      <div className="forgotResetContainer">
+        <div className="forgotPassword">Forgot your password?</div>
+        <div className="resetLink">Reset here</div>
+      </div>
+
+
+      {/* Conditional rendering based on login status */}
+      {loggedIn ? <div>Your email address is {email}</div> : <div />}
+    </div>
+  );
+};
+
+export default Home;
--- src/ManageRoutes/EmployeeRoutes.jsx
+++ src/ManageRoutes/EmployeeRoutes.jsx
@@ -1,16 +1,4 @@
-// import { Route, Routes } from "react-router-dom";
-// import Login from "../Components/Users/Login";
-// import ForgotPassword from "../Components/Users/ForgotPassword";
-// import EmployeeDashboard from "../Components/Employee/EmployeeDashboard";
-// export default function EmployeeRoutes() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<EmployeeDashboard />} />
-//       <Route path="ForgotPassword" element={<ForgotPassword />} />
-//       <Route path="*" element={<EmployeeDashboard />} />
-//     </Routes>
-//   );
-// }
+
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../Components/Users/Login";
