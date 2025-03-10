febe660e00e2c7e4fe5550a2df3ac4f9c92efd4e
nagarajuboda <nagarajuboda014@gmail.com>
2025-03-10 21:37:17 +0530

    'LatestChanges'

--- src/Components/Users/Login.jsx
+++ src/Components/Users/Login.jsx
@@ -75,6 +75,7 @@ const Home = () => {
 
       var result = await responses.data;
       localStorage.setItem("sessionData", JSON.stringify(result.item));
+
       setLoggedIn(true);
       if (result.isSuccess === true) {
         setLoggedIn(true);
@@ -85,6 +86,7 @@ const Home = () => {
           localStorage.setItem("Email", valuess.email);
           navigate("/user/CreateNewPassword");
         } else {
+          localStorage.setItem("token", result.item.token);
           if (result.item.employee.role.name === "US-finance") {
             navigate("/Dashboard/FinanceDashboard");
           } else if (result.item.employee.role.name === "Admin") {
